42df94ebe94ed2abbb8a8827e63d18c5193d9e09
Jessica Orrick <jessica.orrick@outlook.com>
2024-01-21 18:51:39 -0600

    Studio work

--- src/main/java/org/launchcode/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/controllers/SpaDayController.java
@@ -63,6 +63,10 @@ public class SpaDayController {
             }
         }
 
+        model.addAttribute("name", name );
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+
         return "menu";
     }
 }
