4e8feb5a25efdb13c5dc2a13f60112d0678a2767
Jessica Orrick <jessica.orrick@outlook.com>
2024-01-28 08:35:32 -0600

    Studio Complete on user-signup-pt2

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,23 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.*;
+
 public class User {
+
+    @NotBlank(message = "Name is required.")
+    @Size(min = 5, max = 15, message = "Name must be between 5 and 15 characters." )
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
+
 
     public User() {
 
@@ -38,6 +52,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+    }
+
+    private void checkPassword() {
+        if(password != null || verifyPassword != null && !password.equals(verifyPassword)){
+            verifyPassword = null;
+        }
     }
 }
 
